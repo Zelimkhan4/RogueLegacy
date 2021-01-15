e2830229138d1397c471167e201ada82ead1ed54
Zelimkhan4 <vip.guchiev@mail.ru>
2021-01-15 15:15:08 +0300

    Сделал отображение персонажа.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import pygame
+import sys
+import os
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением {fullname} не найден!")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 if __name__ == '__main__':
@@ -7,10 +24,13 @@ if __name__ == '__main__':
     FPS = 60
     clock = pygame.time.Clock()
     running = True
+    image = load_image('knight.png', -1)
+    screen.blit(image, (10, 10))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((255, 255, 255))
+        screen.fill((0, 0, 0))
+        screen.blit(image, (10, 10))
         pygame.display.flip()
         clock.tick()
