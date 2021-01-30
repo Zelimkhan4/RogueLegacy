d68a7a7a27c5298b37034ae2c0cfc9fb19a8d51c
Zelimkhan4 <vip.guchiev@mail.ru>
2021-01-30 15:10:25 +0300

    Add simple level.

--- main.py
+++ main.py
@@ -2,28 +2,67 @@ import pygame
 import sys
 import os
 
+
 class Borders(pygame.sprite.Group):
     def repaint(self):
         for sprite in self.sprites():
             sprite.image.fill((255, 255, 255))
+
+
 pygame.init()
 borders = Borders()
 all_sprites = pygame.sprite.Group()
 GRAVITY = 10
 heros = pygame.sprite.Group()
 
-class Tiles(pygame.sprite.Sprite):
-    def __init__(self, row, col):
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением {fullname} не найден!")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        else:
+            if type(colorkey) == tuple:
+                if colorkey == (-1, -1):
+                    colorkey = image.get_at((image.get_rect().width - 1, image.get_rect().height - 1))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, row, col, width_of_tile, height_of_tile):
         super().__init__(borders)
-        image = pygame.image.load('data/tiles.jpg')
+        image = pygame.transform.scale(pygame.image.load('data/Tiles/Tileset/Ground.png'), (width_of_tile, height_of_tile))
         self.image = image
         self.rect = image.get_rect()
         self.rect.x = col
         self.rect.y = row
-        self.image.fill((0, 0, 0))
 
-    def repaint(self):
-        self.image.fill((0, 0, 0))
+class GrassUp(pygame.sprite.Sprite):
+    def __init__(self, row, col, width_of_tile, height_of_tile):
+        super().__init__(borders)
+        image = pygame.transform.scale(load_image('Tiles/Tileset/GrassUp.png'), (width_of_tile, height_of_tile))
+        self.image = image
+        self.rect = image.get_rect()
+        self.rect.x = col
+        self.rect.y = row
+    
+class GrassLeftSide(pygame.sprite.Sprite):
+    def __init__(self, row, col, width_of_tile, height_of_tile):
+        super().__init__(borders)
+        image = pygame.transform.scale(load_image('Tiles/Tileset/GrassLeftSide.png'), (width_of_tile, height_of_tile))
+        self.image = image
+        self.rect = image.get_rect()
+        self.rect.x = col
+        self.rect.y = row
 
 class Controller:
     def __init__(self, sprite):
@@ -52,8 +91,6 @@ class Character(pygame.sprite.Sprite):
             for border in borderes:
                 if border.rect.y <= self.rect.y + self.rect.height and\
                         self.rect.x + self.rect.width // 2 in range(border.rect.x, border.rect.x + border.rect.width):
-                    border.image.fill((0, 255, 0))
-                    #print('пол')
                     self.rect.y = border.rect.y - self.rect.height + 1
                     self.onGround = True
                     ground_y = border.rect.y
@@ -61,10 +98,7 @@ class Character(pygame.sprite.Sprite):
                          ground_y != self.rect.y:
                     self.rect.x = old_pos.x
                     self.rect.y = border.rect.y - self.rect.height + 1
-                    border.image.fill((255, 0, 0))
-                    #print('стена')
                 else:
-                    border.image.fill((0, 255, 0))
                     self.rect.y = border.rect.y - self.rect.height + 1
 
 def intro(screen):
@@ -80,35 +114,29 @@ def intro(screen):
         pygame.display.flip()
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением {fullname} не найден!")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+
 
 
 def loadLevel(filename):
     with open(filename) as map:
         level = [i.strip('\n') for i in map.readlines()]
-    width_of_tile = width / len(level[0])
-    height_of_tile = height / len(level)
+    level_height = len(level)
+    level_width = len(level[0])
+    width_of_tile = width // level_width
+    height_of_tile = height // level_height
     for rowind, row in enumerate(level):
         for colind, col in enumerate(row):
+            print(col)
             if col == '@':
-                Tiles(rowind * height_of_tile, colind * width_of_tile)
+                Ground(rowind * height_of_tile, colind * width_of_tile, width_of_tile, height_of_tile)
+            elif col == '^':
+                GrassUp(rowind * height_of_tile, colind * width_of_tile, width_of_tile, height_of_tile)
+            elif col == '<':
+                GrassLeftSide(rowind * height_of_tile, colind * width_of_tile, width_of_tile, height_of_tile)
 
 if __name__ == '__main__':
     step = 20
-    size = width, height = 1000, 600
+    size = width, height = 1920, 1080
     screen = pygame.display.set_mode(size)
     FPS = 30
     clock = pygame.time.Clock()
@@ -150,7 +178,7 @@ if __name__ == '__main__':
                 elif event.key == pygame.K_BACKSPACE:
                     hero.image = image
         screen.fill((0, 0, 0))
-
+        screen.blit(pygame.transform.scale(load_image('Tiles/Background/Background.png'), (screen.get_width(), screen.get_height())), (0, 0))
         borders.draw(screen)
         borders.update()
         if not hero.onGround:
@@ -158,7 +186,6 @@ if __name__ == '__main__':
         all_sprites.update()
         all_sprites.draw(screen)
 
-        borders.repaint()
         heros.update(new_pos)
         heros.draw(screen)
 
